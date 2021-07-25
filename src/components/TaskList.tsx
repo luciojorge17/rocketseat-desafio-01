5f4117da62603b6b5f9d5d2a74d5bc3874c6222b
Lucio Jorge <luciojorge17@gmail.com>
2021-07-24 21:29:42 -0300

    Desafio completo

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,37 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle.trim() === '') {
+      alert('Informe um título para a tarefa');
+      return;
+    }
+    setTasks([...tasks, {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        const updatedtask = {
+          ...task,
+          isComplete: !task.isComplete,
+        };
+        return updatedtask;
+      }
+      return task;
+    });
+ 
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
